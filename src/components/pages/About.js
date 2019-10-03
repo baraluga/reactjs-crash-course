4c7df62a4c7470154e61fd3d4b13b3dbd48ac474
baraluga <baperaltaa@gmail.com>
2019-10-03 19:55:07 +0800

    Added Header layout and About page

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the the TodoList app v1. A part of a ReactJS Crash Course
+        Tutorial
+      </p>
+    </React.Fragment>
+  );
+}
