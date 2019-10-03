9eae8a5d8f4bdf47204f8fc2f7268c34c4aaaa9f
baraluga <baperaltaa@gmail.com>
2019-10-03 19:56:09 +0800

    Integrated AddTodo, Header, and About routing; Integrated with JSONPlaceholder REST API

--- src/App.js
+++ src/App.js
@@ -1,40 +1,62 @@
+import Axios from 'axios';
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Todos from './components/Todos';
-
+import { About, AddTodo, Header, Todos } from './components';
+import { GET_TODOS_URL, UPDATE_TODO_URL } from './Constants';
 
 class App extends Component {
   state = {
-    todos: [
-      { id: 1, title: 'Take out the trash', completed: false },
-      { id: 2, title: 'Dinner with Maj', completed: false },
-      { id: 3, title: 'Meeting with Yannick', completed: false },
-    ]
-  }
+    todos: [],
+  };
 
-  toggleCompletion = (id) =>
+  toggleCompletion = id =>
     this.setState({
       todos: this.state.todos.map(todo => ({
         ...todo,
-        completed: id === todo.id ? !todo.completed : todo.completed
-      }))
+        completed: id === todo.id ? !todo.completed : todo.completed,
+      })),
     });
 
+  deleteItem = id =>
+    Axios.delete(UPDATE_TODO_URL + id).then(res => console.log(res));
 
-  deleteItem = (id) =>
-    this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id)
-    });
-
+  addTodo = title =>
+    Axios.post(UPDATE_TODO_URL, {
+      title,
+      completed: false,
+    }).then(res => this.setState({ todos: [...this.state.todos, res.data] }));
 
   render = () => {
     return (
-      <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.toggleCompletion}
-          delete={this.deleteItem} />
-      </div>
-    )
-  }
+      <BrowserRouter>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <React.Fragment>
+                  <AddTodo addTodo={this.addTodo} />
+                  <Todos
+                    todos={this.state.todos}
+                    markComplete={this.toggleCompletion}
+                    delete={this.deleteItem}
+                  />
+                </React.Fragment>
+              )}
+            />
+            <Route path="/about" component={About}></Route>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  };
+
+  componentDidMount = () => {
+    Axios.get(GET_TODOS_URL).then(res => this.setState({ todos: res.data }));
+  };
 }
 
 export default App;
